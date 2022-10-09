a05ce4c4dc7d67d2f6766c9d935ce9483a1a9998
Ivan <Streafyhs@gmail.com>
2022-10-09 22:31:54 +0700

    Tree implementation without remove operation

--- task_1_2_2/src/main/java/ru/nsu/fit/Main.java
+++ task_1_2_2/src/main/java/ru/nsu/fit/Main.java
@@ -3,21 +3,18 @@ package ru.nsu.fit;
 public class Main {
     public static void main(String[] args) {
         Tree<String> tree = new Tree<>("root");
-        Tree<String> tree1 = tree.add1("A");
-        Tree<String> tree2 = tree.add1("B");
-        Tree<String> tree3 = tree.add1("C");
-        tree.add1("D");
-        Tree<String> tree4 = tree.add1(tree1, "AB");
-        tree.add1(tree2, "BB");
-        tree.add1(tree3, "CB");
+        Tree<String> tree1 = tree.add("A");
+        Tree<String> tree2 = tree.add("B");
+        Tree<String> tree3 = tree.add("C");
+        tree.add("D");
+        Tree<String> tree4 = tree.add(tree1, "AB");
+        tree.add(tree2, "BB");
+        tree.add(tree3, "CB");
         System.out.println("Printing test Tree");
-        tree.add1(tree4, "ABC");
-        tree.printTree();
-        tree.remove1("A");
-        tree.printTree();
+        tree.add(tree4, "ABC");
 
-        Tree<String> test = new Tree<>();
-        test.add1("1");
-        test.add1("2");
+        for (String s : tree) {
+            System.out.println(s);
+        }
     }
 }
