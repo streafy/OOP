11669bb710b708963c6479d04b6c63ea67d8db8a
Ivan <Streafyhs@gmail.com>
2022-11-20 22:11:51 +0700

    Aho-Corasick algorithm implementation

--- task_1_3_1/src/main/java/ru/nsu/fit/Main.java
+++ task_1_3_1/src/main/java/ru/nsu/fit/Main.java
@@ -1,13 +1,10 @@
 package ru.nsu.fit;
 
+import ru.nsu.fit.algorithms.AhoCorasick;
+
 public class Main {
     public static void main(String[] args) {
-        String pattern = "test";
-        String text = "This is a test string";
-
-        SubstringFinder sf = new SubstringFinder(pattern);
-
-        int index = sf.search(text);
-        System.out.println(index);
+        AhoCorasick ah = new AhoCorasick();
+        ah.processText("qweqwe bebebe", "be");
     }
 }
