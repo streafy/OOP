922b763a871e3cc300f43239831d505fe0626f34
streafy <streafy@users.noreply.github.com>
2022-12-05 16:36:15 +0000

    Deploying to gh-pages from @ streafy/OOP@1cd295620d7066d7ee3842f4d85798167e1bfe1b 🚀

---
+++ task_1_5_1/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"ru.nsu.fit","c":"Calculator","l":"Calculator()","url":"%3Cinit%3E()"},{"p":"ru.nsu.fit","c":"Calculator","l":"evaluate(String)","url":"evaluate(java.lang.String)"}]
