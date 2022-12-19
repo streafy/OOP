e18b2ceb01a914f28751637e80d5eb86f9431745
streafy <streafy@users.noreply.github.com>
2022-12-19 08:43:30 +0000

    Deploying to gh-pages from @ streafy/OOP@aa09b61e5723963b760143affac031d1b52b22e1 🚀

--- task_1_4_1/type-search-index.js
+++ task_1_4_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.fit","l":"AttestationForm"},{"p":"ru.nsu.fit","l":"Grade"},{"p":"ru.nsu.fit","l":"GradeBook"},{"p":"ru.nsu.fit","l":"Subject"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.fit","l":"AttestationForm"},{"p":"ru.nsu.fit","l":"CreditGrade"},{"p":"ru.nsu.fit","l":"DifCreditGrade"},{"p":"ru.nsu.fit","l":"ExamGrade"},{"p":"ru.nsu.fit","l":"Grade"},{"p":"ru.nsu.fit","l":"GradeBook"},{"p":"ru.nsu.fit","l":"Subject"}]
