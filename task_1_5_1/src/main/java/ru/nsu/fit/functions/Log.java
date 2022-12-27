9b9f880ed787889c5b197d58c7fd032e2b4068a8
Ivan <Streafyhs@gmail.com>
2022-12-28 06:32:44 +0700

    completed implementation with Function classes

---
+++ task_1_5_1/src/main/java/ru/nsu/fit/functions/Log.java
@@ -0,0 +1,37 @@
+package ru.nsu.fit.functions;
+
+import java.util.List;
+
+public class Log implements Function {
+    /**
+     * Returns arity of Function.
+     *
+     * @return arity of Function
+     */
+    @Override
+    public int getArity() {
+        return 1;
+    }
+
+    /**
+     * Parses function from token.
+     *
+     * @param token String token
+     * @return Function if it can be parsed from token, null otherwise
+     */
+    @Override
+    public Function parse(String token) {
+        return token.equals("log") ? new Log() : null;
+    }
+
+    /**
+     * Executes Function.
+     *
+     * @param arguments Function arguments
+     * @return Number with value equals to result of Function
+     */
+    @Override
+    public Number execute(List<Number> arguments) {
+        return new Number(Math.log(arguments.get(0).value()));
+    }
+}
