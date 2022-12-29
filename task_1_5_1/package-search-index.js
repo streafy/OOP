52c2ba9b56c5b959c4f6ad79829c44353ccadd19
streafy <streafy@users.noreply.github.com>
2022-12-29 10:42:21 +0000

    Deploying to gh-pages from @ streafy/OOP@ddbc64257c572232dbd13ad462a0badd3b1e8f14 🚀

--- task_1_5_1/package-search-index.js
+++ task_1_5_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.fit"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.fit"},{"l":"ru.nsu.fit.functions"}]
