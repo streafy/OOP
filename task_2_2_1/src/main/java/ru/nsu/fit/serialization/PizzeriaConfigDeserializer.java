8921e979ed48e6935456112968dc02ba99804367
Ivan <Streafyhs@gmail.com>
2023-03-28 11:21:28 +0700

    added PizzeriaConfig deserialization

---
+++ task_2_2_1/src/main/java/ru/nsu/fit/serialization/PizzeriaConfigDeserializer.java
@@ -0,0 +1,27 @@
+package ru.nsu.fit.serialization;
+
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class PizzeriaConfigDeserializer {
+
+    private final Gson gson;
+
+    public PizzeriaConfigDeserializer() {
+        this.gson = new Gson();
+    }
+
+    public PizzeriaConfig deserialize(String filename) {
+        InputStream configInputStream = this.getClass()
+                .getResourceAsStream(filename);
+        if (configInputStream == null) {
+            throw new RuntimeException("Config file not found");
+        }
+        JsonReader reader = new JsonReader(new InputStreamReader(configInputStream));
+        return gson.fromJson(reader,
+                PizzeriaConfig.class);
+    }
+}
