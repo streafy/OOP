f23b1ffdae2c628bb0d804d0d9ff25508380a63e
Ivan <Streafyhs@gmail.com>
2023-04-04 01:56:38 +0700

    added tests for soft and forced shutdowns

--- task_2_2_1/src/test/java/ru/nsu/fit/PizzeriaTest.java
+++ task_2_2_1/src/test/java/ru/nsu/fit/PizzeriaTest.java
@@ -1,6 +1,7 @@
 package ru.nsu.fit;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.nsu.fit.pizzeria_implementation.order.Order;
 import ru.nsu.fit.pizzeria_implementation.order.OrderStatus;
@@ -14,17 +15,21 @@ import java.util.concurrent.TimeUnit;
 
 public class PizzeriaTest {
 
-    @Test
-    public void ordersCompletionTest() {
+    private Pizzeria pizzeria;
+
+    @BeforeEach
+    public void init() {
         SynchronousQueue<Order> orderQueue = new SynchronousQueue<>();
 
         PizzeriaConfigDeserializer deserializer = new PizzeriaConfigDeserializer();
         PizzeriaConfig config = deserializer.deserialize("/config.json");
 
-        Pizzeria pizzeria = new PizzeriaImpl(config, orderQueue);
-
+        pizzeria = new PizzeriaImpl(config, orderQueue);
         pizzeria.startWorking();
+    }
 
+    @Test
+    public void ordersCompletionTest() {
         Order order1 = new Order(1, 1);
         pizzeria.orderPizza(order1);
         Order order2 = new Order(2, 1);
@@ -39,18 +44,50 @@ public class PizzeriaTest {
         pizzeria.orderPizza(order6);
 
         try {
-            TimeUnit.SECONDS.sleep(40);
+            TimeUnit.SECONDS.sleep(30);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
-        Assertions.assertEquals(order1.getStatus(), OrderStatus.DELIVERED);
-        Assertions.assertEquals(order2.getStatus(), OrderStatus.DELIVERED);
-        Assertions.assertEquals(order3.getStatus(), OrderStatus.DELIVERED);
-        Assertions.assertEquals(order4.getStatus(), OrderStatus.DELIVERED);
-        Assertions.assertEquals(order5.getStatus(), OrderStatus.DELIVERED);
-        Assertions.assertEquals(order6.getStatus(), OrderStatus.DELIVERED);
+        Assertions.assertEquals(OrderStatus.DELIVERED, order1.getStatus());
+        Assertions.assertEquals(OrderStatus.DELIVERED, order2.getStatus());
+        Assertions.assertEquals(OrderStatus.DELIVERED, order3.getStatus());
+        Assertions.assertEquals(OrderStatus.DELIVERED, order4.getStatus());
+        Assertions.assertEquals(OrderStatus.DELIVERED, order5.getStatus());
+        Assertions.assertEquals(OrderStatus.DELIVERED, order6.getStatus());
 
         pizzeria.stopWorkingForced();
+    }
+
+    @Test
+    public void forcedShutDownTest() {
+        Order order1 = new Order(1, 1);
+        pizzeria.orderPizza(order1);
+
+        pizzeria.stopWorkingForced();
+
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        Assertions.assertEquals(OrderStatus.AWAITING_DELIVERY, order1.getStatus());
+    }
+
+    @Test
+    public void softShutdownTest() {
+        Order order1 = new Order(1, 1);
+        pizzeria.orderPizza(order1);
+
+        pizzeria.stopWorkingSoft();
+
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        Assertions.assertEquals(OrderStatus.DELIVERED, order1.getStatus());
     }
 }
