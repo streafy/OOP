14863d936de0055c44057be968c564f810e069bc
Ivan <Streafyhs@gmail.com>
2023-04-18 03:02:34 +0700

    changed JavafxPresenter: now handles pressing keys, game score and status updates

--- task_2_3_1/src/main/java/ru/nsu/fit/presenter/JavafxPresenter.java
+++ task_2_3_1/src/main/java/ru/nsu/fit/presenter/JavafxPresenter.java
@@ -1,35 +1,65 @@
 package ru.nsu.fit.presenter;
 
 import javafx.animation.AnimationTimer;
+import javafx.scene.input.KeyCode;
+import ru.nsu.fit.model.Game;
 import ru.nsu.fit.model.Snake;
+import ru.nsu.fit.model.utils.GameStatus;
+import ru.nsu.fit.model.utils.SnakeDirection;
 import ru.nsu.fit.view.GameFieldView;
 
 public class JavafxPresenter implements Presenter {
 
-    private GameFieldView gameFieldView;
-    private Snake snake;
+    private final GameFieldView gameFieldView;
+    private final Game game;
+    private AnimationTimer timer;
+    private final Snake snake;
 
-    public JavafxPresenter(GameFieldView gameFieldView, Snake snake) {
+    public JavafxPresenter(GameFieldView gameFieldView, Game game) {
         this.gameFieldView = gameFieldView;
-        this.snake = snake;
-    }
+        this.game = game;
 
-    public void onKeyboardButtonPressed() {
-
-    }
-
-    public void onFoodEaten() {
-
+        snake = game.getSnake();
     }
 
     public void startGameLoop() {
-        new AnimationTimer() {
+        gameFieldView.getScene()
+                     .setOnKeyPressed(event -> handleKeyPressed(event.getCode()));
+
+        timer = new AnimationTimer() {
+            private long lastUpdate = 0;
+
             @Override
             public void handle(long now) {
-                snake.moveUp();
-                gameFieldView.renderGameField();
-                gameFieldView.renderSnake(snake.getSnakeLocation());
+                if (now - lastUpdate >= 200_000_000) {
+                    lastUpdate = now;
+
+                    updateGame();
+                }
             }
-        }.start();
+        };
+        timer.start();
+    }
+
+    public void updateGame() {
+        if (game.getGameStatus() == GameStatus.GAME_OVER) {
+            gameFieldView.showGameOverScreen(game.getScore());
+            timer.stop();
+            return;
+        }
+
+        gameFieldView.showScore(game.getScore());
+        game.moveSnake();
+
+        gameFieldView.renderGameField(snake.getBody(), game.getFood().getPoint());
+    }
+
+    public void handleKeyPressed(KeyCode keyCode) {
+        switch (keyCode) {
+            case UP -> snake.setCurrentDirection(SnakeDirection.UP);
+            case DOWN -> snake.setCurrentDirection(SnakeDirection.DOWN);
+            case LEFT -> snake.setCurrentDirection(SnakeDirection.LEFT);
+            case RIGHT -> snake.setCurrentDirection(SnakeDirection.RIGHT);
+        }
     }
 }
