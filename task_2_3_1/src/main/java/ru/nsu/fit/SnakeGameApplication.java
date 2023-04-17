8625625c6adfb546fe9c4d77e4df13c8ad719699
Ivan <Streafyhs@gmail.com>
2023-04-18 03:03:21 +0700

    minor changes to SnakeGameApplication

--- task_2_3_1/src/main/java/ru/nsu/fit/SnakeGameApplication.java
+++ task_2_3_1/src/main/java/ru/nsu/fit/SnakeGameApplication.java
@@ -2,10 +2,11 @@ package ru.nsu.fit;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import ru.nsu.fit.model.Food;
+import ru.nsu.fit.model.Game;
 import ru.nsu.fit.model.Snake;
 import ru.nsu.fit.presenter.JavafxPresenter;
 import ru.nsu.fit.view.GameFieldView;
@@ -17,17 +18,20 @@ public class SnakeGameApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(SnakeGameApplication.class.getResource("/ru/nsu/fit/game-field-view.fxml"));
-        HBox rootContainer = fxmlLoader.load();
+        Pane rootContainer = fxmlLoader.load();
         Scene scene = new Scene(rootContainer, 800, 500);
         stage.setTitle("SnakeGame");
         stage.setScene(scene);
         stage.show();
 
-        GameFieldView gameFieldView = new GameFieldView(scene);
         Snake snake = new Snake();
-        JavafxPresenter javafxPresenter = new JavafxPresenter(gameFieldView, snake);
+        Food food = new Food();
+        Game game = new Game(10, 10, snake, food);
 
-        gameFieldView.renderSnake(snake.getSnakeLocation());
+        GameFieldView gameFieldView = new GameFieldView(scene);
+
+        JavafxPresenter javafxPresenter = new JavafxPresenter(gameFieldView, game);
+
         javafxPresenter.startGameLoop();
     }
 
