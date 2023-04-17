cf601fc3b92b9f20229855dd8a4b461de61d21ef
Ivan <Streafyhs@gmail.com>
2023-04-18 03:00:46 +0700

    changed GameFieldView: now Shows score and game over text

--- task_2_3_1/src/main/java/ru/nsu/fit/view/GameFieldView.java
+++ task_2_3_1/src/main/java/ru/nsu/fit/view/GameFieldView.java
@@ -1,35 +1,55 @@
 package ru.nsu.fit.view;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 import java.awt.*;
 import java.util.List;
-import java.util.stream.IntStream;
 
 public class GameFieldView {
 
-    private HBox rootContainer;
-
     private static final int DEFAULT_ROW_COUNT = 10;
     private static final int DEFAULT_COL_COUNT = 10;
+
+    private final Scene scene;
+    private final Pane rootContainer;
+    private final HBox gameFieldContainer;
+
+    private final int rowCount;
+    private final int colCount;
+
+    private final Label gameInfoLabel;
 
     public GameFieldView(Scene scene) {
         this(scene, DEFAULT_ROW_COUNT, DEFAULT_COL_COUNT);
     }
 
     public GameFieldView(Scene scene, int rowCount, int colCount) {
-        rootContainer = (HBox) scene.getRoot();
+        this.scene = scene;
+        this.rowCount = rowCount;
+        this.colCount = colCount;
+
+        rootContainer = (Pane) scene.getRoot();
+        gameFieldContainer = (HBox) rootContainer.getChildren().get(0);
+
+        gameInfoLabel = (Label) rootContainer.lookup("#gameInfoLabel");
+
         initGameField(rowCount, colCount);
+    }
+
+    public Scene getScene() {
+        return scene;
     }
 
     private void initGameField(int rowCount, int colCount) {
         for (int i = 0; i < colCount; i++) {
             VBox rowContainer = new VBox();
-            rootContainer.getChildren().add(rowContainer);
+            gameFieldContainer.getChildren().add(rowContainer);
             for (int j = 0; j < rowCount; j++) {
                 Rectangle rectangle = new Rectangle(50, 50);
                 rectangle.setFill(Color.CADETBLUE);
@@ -38,22 +58,31 @@ public class GameFieldView {
         }
     }
 
-    //TODO: change to not rerender all game field
-    public void renderGameField() {
-        for (int x = 0; x < DEFAULT_ROW_COUNT; x++) {
-            for (int y = 0; y < DEFAULT_COL_COUNT; y++) {
+    public void renderGameField(List<Point> snakeBody, Point food) {
+        for (int x = 0; x < rowCount; x++) {
+            for (int y = 0; y < colCount; y++) {
                 changeCellColor(x, y, Color.CADETBLUE);
             }
         }
+        renderSnake(snakeBody);
+        changeCellColor(food.x, food.y, Color.RED);
     }
 
     public void renderSnake(List<Point> snakeCoordinates) {
-        snakeCoordinates.forEach(point ->  changeCellColor(point.x, point.y, Color.GREEN));
+        snakeCoordinates.forEach(point -> changeCellColor(point.x, point.y, Color.GREEN));
     }
 
-    public void changeCellColor(int x, int y, Color color) {
-        VBox column = (VBox) rootContainer.getChildren().get(x);
+    private void changeCellColor(int x, int y, Color color) {
+        VBox column = (VBox) gameFieldContainer.getChildren().get(x);
         Rectangle cell = (Rectangle) column.getChildren().get(y);
         cell.setFill(color);
+    }
+
+    public void showScore(int score) {
+        gameInfoLabel.setText("Score: " + score);
+    }
+
+    public void showGameOverScreen(int score) {
+        gameInfoLabel.setText("Game Over!\nYour Score: " + score);
     }
 }
