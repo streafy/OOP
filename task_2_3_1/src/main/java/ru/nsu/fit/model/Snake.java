0a582a93d04351f94cb5502c003f354aed38f4d6
Ivan <Streafyhs@gmail.com>
2023-04-18 02:57:47 +0700

    changed Snake: now can move in any direction and eat food

--- task_2_3_1/src/main/java/ru/nsu/fit/model/Snake.java
+++ task_2_3_1/src/main/java/ru/nsu/fit/model/Snake.java
@@ -1,38 +1,54 @@
 package ru.nsu.fit.model;
 
-import ru.nsu.fit.model.utils.SnakeMoveDirection;
+import ru.nsu.fit.model.utils.SnakeDirection;
 
-import java.awt.Point;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Snake {
 
-    private static final int DEFAULT_SNAKE_SIZE = 2;
     private static final int DEFAULT_SNAKE_SPAWN_COORDINATE_X = 4;
     private static final int DEFAULT_SNAKE_SPAWN_COORDINATE_Y = 4;
-    private static final SnakeMoveDirection DEFAULT_SNAKE_DIRECTION = SnakeMoveDirection.TOP;
 
-    private final List<Point> snakeLocation = new ArrayList<>();
-    private int snakeLength;
+    private final List<Point> body = new ArrayList<>();
+
+    private SnakeDirection currentDirection = SnakeDirection.UP;
 
     public Snake() {
-        snakeLength = DEFAULT_SNAKE_SIZE;
-        snakeLocation.add(new Point(DEFAULT_SNAKE_SPAWN_COORDINATE_X, DEFAULT_SNAKE_SPAWN_COORDINATE_Y));
-        snakeLocation.add(new Point(DEFAULT_SNAKE_SPAWN_COORDINATE_X, DEFAULT_SNAKE_SPAWN_COORDINATE_Y - 1));
+
+        body.add(new Point(DEFAULT_SNAKE_SPAWN_COORDINATE_X, DEFAULT_SNAKE_SPAWN_COORDINATE_Y));
+        body.add(new Point(DEFAULT_SNAKE_SPAWN_COORDINATE_X, DEFAULT_SNAKE_SPAWN_COORDINATE_Y + 1));
     }
 
-    public List<Point> getSnakeLocation() {
-        return snakeLocation;
+    public List<Point> getBody() {
+        return body;
     }
 
-    public void moveUp() {
-        snakeLocation.forEach(point -> {
-            if (point.y == 0) {
-                point.y = 9;
-            } else {
-                point.y--;
-            }
-        });
+    public Point getHead() {
+        return body.get(0);
+    }
+
+    public SnakeDirection getCurrentDirection() {
+        return currentDirection;
+    }
+
+    public void setCurrentDirection(SnakeDirection currentDirection) {
+        this.currentDirection = currentDirection;
+    }
+
+    public void move(Point newHead) {
+        body.add(0, newHead);
+        body.remove(body.size() - 1);
+    }
+
+    public void eatFood(Point newHead) {
+        body.add(0, newHead);
+        System.out.println(body.size());
+    }
+
+    public Point getNewHead() {
+        Point oldHead = getHead();
+        return new Point(oldHead.x + currentDirection.getDx(), oldHead.y + currentDirection.getDy());
     }
 }
