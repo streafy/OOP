555cdaf76b8f9c09066683de270a4b24bcf36f0f
Ivan <Streafyhs@gmail.com>
2023-04-18 02:58:27 +0700

    changed Food: can get food coordinates now

--- task_2_3_1/src/main/java/ru/nsu/fit/model/Food.java
+++ task_2_3_1/src/main/java/ru/nsu/fit/model/Food.java
@@ -7,13 +7,17 @@ public class Food {
     private static final int DEFAULT_FOOD_COORDINATE_X = 2;
     private static final int DEFAULT_FOOD_COORDINATE_Y = 3;
 
-    private final Point location;
+    private final Point coordinates;
 
     public Food() {
-        this.location = new Point(DEFAULT_FOOD_COORDINATE_X, DEFAULT_FOOD_COORDINATE_Y);
+        this.coordinates = new Point(DEFAULT_FOOD_COORDINATE_X, DEFAULT_FOOD_COORDINATE_Y);
     }
 
     public Food(int x, int y) {
-        this.location = new Point(x, y);
+        this.coordinates = new Point(x, y);
+    }
+
+    public Point getPoint() {
+        return coordinates;
     }
 }
