8085180cf68cfaee5b003f7e9320794ad3c216d3
Ivan <Streafyhs@gmail.com>
2023-05-02 01:46:10 +0700

    added ConsoleGameFiledView implementation

---
+++ task_2_3_1/src/main/java/ru/nsu/fit/view/console/ConsoleGameFiledView.java
@@ -0,0 +1,42 @@
+package ru.nsu.fit.view.console;
+
+import ru.nsu.fit.view.GameFieldView;
+
+import java.awt.*;
+import java.util.List;
+
+public class ConsoleGameFiledView implements GameFieldView {
+
+    private static final int DEFAULT_ROW_COUNT = 20;
+    private static final int DEFAULT_COL_COUNT = 20;
+
+    private final int rowCount;
+    private final int colCount;
+
+    public ConsoleGameFiledView() {
+        this(DEFAULT_ROW_COUNT, DEFAULT_COL_COUNT);
+    }
+
+    public ConsoleGameFiledView(int rowCount, int colCount) {
+        this.rowCount = rowCount;
+        this.colCount = colCount;
+    }
+
+    @Override
+    public void renderGameField(List<Point> snakeBody, Point food) {
+        for (int x = 0; x < colCount; x++) {
+            for (int y = 0; y < rowCount; y++) {
+                Point point = new Point(x, y);
+                String pointSymbol = ".";
+                if (point.equals(food)) {
+                    pointSymbol = "*";
+                }
+                if (snakeBody.stream().anyMatch(point::equals)) {
+                    pointSymbol = "#";
+                }
+                System.out.print(pointSymbol);
+            }
+            System.out.println();
+        }
+    }
+}
