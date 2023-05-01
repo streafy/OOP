a2a3254d23c4a75743da4bc7a6c3f2d773f9c2b4
Ivan <Streafyhs@gmail.com>
2023-05-02 01:46:47 +0700

    added partial ConsolePresenter implementation

--- task_2_3_1/src/main/java/ru/nsu/fit/presenter/ConsolePresenter.java
+++ task_2_3_1/src/main/java/ru/nsu/fit/presenter/ConsolePresenter.java
@@ -1,4 +1,22 @@
 package ru.nsu.fit.presenter;
 
+import ru.nsu.fit.model.Game;
+import ru.nsu.fit.model.utils.GameStatus;
+import ru.nsu.fit.view.console.ConsoleGameFiledView;
+
 public class ConsolePresenter implements Presenter {
+
+    private final ConsoleGameFiledView gameFiledView;
+    private final Game game;
+
+    public ConsolePresenter(ConsoleGameFiledView gameFiledView, Game game) {
+        this.gameFiledView = gameFiledView;
+        this.game = game;
+    }
+
+    public void startGameLoop() {
+        while (game.getGameStatus() != GameStatus.GAME_OVER) {
+
+        }
+    }
 }
